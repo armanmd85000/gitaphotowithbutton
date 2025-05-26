222f9bb01e000670e5d035a233c44b987598763f
trishi880988 <gauravyt8803@gmail.com>
2025-05-27 01:58:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Get these from https://my.telegram.org
-API_ID = 1234567
-API_HASH = "your_api_hash_here"
+API_ID = 20219694
+API_HASH = "29d9b3a01721ab452fcae79346769e29"
 
 # Get from @BotFather
-BOT_TOKEN = "your_bot_token_here"
+BOT_TOKEN = "7942215521:AAG5Zardlr7ULt2-yleqXeKjHKp4AQtVzd8"
