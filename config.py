7495514a037e756738a2d4ca372c16fcd16f4c1a
armanmd85000 <154997050+armanmd85000@users.noreply.github.com>
2025-08-04 02:45:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Get these from https://my.telegram.org
 API_ID = 25570420
-API_HASH = "6591643fa39b5b9d0eb78cb24db17f69"
+API_HASH = ""
 
 # Get from @BotFather
-BOT_TOKEN = "7266694108:AAHQOIAI19gcG7Axpil1DAmDdl0aPYVNY-w"
+BOT_TOKEN = ""
