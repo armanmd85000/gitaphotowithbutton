6e6ce044ecc5d7dfd2829caac079d0dbc5347558
armanmd85000 <154997050+armanmd85000@users.noreply.github.com>
2025-08-21 20:15:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 # Get these from https://my.telegram.org
-API_ID = 25570420
-API_HASH = ""
+API_ID = int(os.environ.get("API_ID", "0"))
+API_HASH = os.environ.get("API_HASH", "")
 
 # Get from @BotFather
-BOT_TOKEN = ""
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
